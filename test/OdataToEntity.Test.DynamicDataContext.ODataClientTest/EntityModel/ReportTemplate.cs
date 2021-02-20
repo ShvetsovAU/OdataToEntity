458c0c33f1281bf1b1b775306a84027c8bf48319
20912088 <an.shvetsov@ase-ec.ru>
2021-02-20 16:12:25 +0300

    ReportTemplate entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ReportTemplate.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ReportTemplate.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class ReportTemplate
+    #region scaffold model
+
+    //public partial class ReportTemplate
+    //{
+    //    public short Id { get; set; }
+    //    public string Name { get; set; }
+    //    public byte[] Data { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    /// <summary>
+    /// Сущность для хранения шаблона отчета
+    /// </summary>
+    public class ReportTemplate
     {
-        public short Id { get; set; }
+        public short Id { get; set; }  //public Int16 Id { get; set; }
+
+        /// <summary>
+        /// Название шаблона для отображения
+        /// </summary>
+        [MaxLength(100)]
         public string Name { get; set; }
+        /// <summary>
+        /// Сериализованный шаблон mhd
+        /// </summary>
+#warning varbinary size
         public byte[] Data { get; set; }
     }
 }
