001a4bdf749b2da2088e1376318915dcd47a60f2
ShvetsovAu <auskst@gmail.com>
2021-02-20 11:51:41 +0300

    Indicator entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/Indicator.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/Indicator.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class Indicator
+    #region scaffold model
+
+    //public partial class Indicator
+    //{
+    //    public int ObjectId { get; set; }
+    //    public string Name { get; set; }
+    //    public int Type { get; set; }
+    //    public int Color { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    public class Indicator : IEntity
     {
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
         public string Name { get; set; }
         public int Type { get; set; }
