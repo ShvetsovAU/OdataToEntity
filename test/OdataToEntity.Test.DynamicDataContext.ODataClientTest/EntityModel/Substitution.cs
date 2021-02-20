fe2362af87bf0623cf3f423059963bc9f74836a5
20912088 <an.shvetsov@ase-ec.ru>
2021-02-20 17:13:13 +0300

    Substitution entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/Substitution.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/Substitution.cs
@@ -1,17 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class Substitution
-    {
-        public int ObjectId { get; set; }
-        public int AttributeId { get; set; }
-        public string OriginalValue { get; set; }
-        public string SubstituteValue { get; set; }
+    #region scaffold model
 
-        public virtual P3DBAttribute Attribute { get; set; }
+    //public partial class Substitution
+    //{
+    //    public int ObjectId { get; set; }
+    //    public int AttributeId { get; set; }
+    //    public string OriginalValue { get; set; }
+    //    public string SubstituteValue { get; set; }
+
+    //    public virtual P3DBAttribute Attribute { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    /// <summary>
+    /// Таблица подстановок
+    /// </summary>
+    public partial class Substitution : IEntity
+    {
+        [Key]
+        [Required]
+        public int ObjectId { get; set; }
+
+        public int AttributeId { get; set; }
+        /// <summary>
+        /// Атрибут 3д модели
+        /// </summary>
+        [ForeignKey("AttributeId")]
+        [Required]
+        public P3DBAttribute Attribute { get; set; }
+
+        /// <summary>
+        /// Значение атрибута
+        /// </summary>
+        public string OriginalValue { get; set; }
+
+        /// <summary>
+        /// Значение подстановки
+        /// </summary>
+        public string SubstituteValue { get; set; }
     }
 }
