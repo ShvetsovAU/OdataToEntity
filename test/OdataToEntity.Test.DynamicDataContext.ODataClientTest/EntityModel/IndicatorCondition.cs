721f11971649bdfaf1bb4315333b44fb90e88ac6
ShvetsovAu <auskst@gmail.com>
2021-02-20 11:53:25 +0300

    IndicatorCondition entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/IndicatorCondition.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/IndicatorCondition.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class IndicatorCondition
+    #region scaffold model
+
+    //public partial class IndicatorCondition
+    //{
+    //    public int ObjectId { get; set; }
+    //    public int IndicatorObjectId { get; set; }
+    //    public string Name { get; set; }
+    //    public int Priority { get; set; }
+    //    public string Condition { get; set; }
+    //    public bool CheckInActivity { get; set; }
+    //    public int Color { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    public class IndicatorCondition : IEntity
     {
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
         public int IndicatorObjectId { get; set; }
         public string Name { get; set; }
