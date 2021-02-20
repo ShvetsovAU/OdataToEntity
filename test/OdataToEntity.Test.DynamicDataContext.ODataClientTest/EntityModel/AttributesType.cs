48a4a2d0a6eceada6d9f4502fb72d5598e412c4f
ShvetsovAu <auskst@gmail.com>
2021-02-20 10:10:49 +0300

    AttributesType entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/AttributesType.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/AttributesType.cs
@@ -1,27 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class AttributesType
+    #region scaffold model
+
+    //public partial class AttributesType
+    //{
+    //    public AttributesType()
+    //    {
+    //        WorkTaskAttributeValues = new HashSet<WorkTaskAttributeValue>();
+    //    }
+
+    //    public int ObjectId { get; set; }
+    //    public string Name { get; set; }
+    //    public int Type { get; set; }
+    //    public bool ReadOnly { get; set; }
+    //    public bool Required { get; set; }
+    //    public bool IsGlobal { get; set; }
+    //    public string DefaultValue { get; set; }
+    //    public string AvailableValues { get; set; }
+    //    public string Alias { get; set; }
+
+    //    public virtual ICollection<WorkTaskAttributeValue> WorkTaskAttributeValues { get; set; }
+    //}
+    #endregion scaffold model
+
+    public partial class AttributesType : IEntity
     {
         public AttributesType()
         {
-            WorkTaskAttributeValues = new HashSet<WorkTaskAttributeValue>();
+            this.WorkTaskAttributeValues = new List<WorkTaskAttributeValue>();
         }
 
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public int Type { get; set; }
+
+        [Required]
         public bool ReadOnly { get; set; }
+
+        [Required]
         public bool Required { get; set; }
+
+        [Required]
         public bool IsGlobal { get; set; }
+
         public string DefaultValue { get; set; }
+
         public string AvailableValues { get; set; }
+
         public string Alias { get; set; }
 
         public virtual ICollection<WorkTaskAttributeValue> WorkTaskAttributeValues { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is AttributesType entity))
+                return false;
+
+            return entity.ObjectId == ObjectId;
+        }
     }
 }
