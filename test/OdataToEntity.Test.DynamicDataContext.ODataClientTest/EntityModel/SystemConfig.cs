4d61c5a2c9a86e471ac17755319c9a5de5da19ef
20912088 <an.shvetsov@ase-ec.ru>
2021-02-20 17:44:28 +0300

    SystemConfig entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/SystemConfig.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/SystemConfig.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class SystemConfig
+    #region scaffold model
+
+    //public partial class SystemConfig
+    //{
+    //    public int ObjectId { get; set; }
+    //    public string Name { get; set; }
+    //    public string Value { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    public partial class SystemConfig : IEntity
     {
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
+
+        [MaxLength(50)]
+        //[Index(IsUnique = true)] //Перенес в NSZDbContext
         public string Name { get; set; }
+
         public string Value { get; set; }
     }
 }
