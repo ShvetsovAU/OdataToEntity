73dd95c89734a1d4fa856227af6d70c0bc41a0fd
ShvetsovAu <auskst@gmail.com>
2021-02-20 09:05:38 +0300

    ActivityTemplate entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ActivityTemplate.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ActivityTemplate.cs
@@ -1,19 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class ActivityTemplate
-    {
-        public ActivityTemplate()
-        {
-            ActivityTypes = new HashSet<ActivityType>();
-        }
+    #region scaffold model
 
+    //public partial class ActivityTemplate
+    //{
+    //    public ActivityTemplate()
+    //    {
+    //        ActivityTypes = new HashSet<ActivityType>();
+    //    }
+
+    //    public int ObjectId { get; set; }
+    //    public string Name { get; set; }
+
+    //    public virtual ICollection<ActivityType> ActivityTypes { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    public class ActivityTemplate : IEntity
+    {
+        private string mName;
+
+        [Key, Required]
         public int ObjectId { get; set; }
-        public string Name { get; set; }
+
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+        }
 
         public virtual ICollection<ActivityType> ActivityTypes { get; set; }
     }
