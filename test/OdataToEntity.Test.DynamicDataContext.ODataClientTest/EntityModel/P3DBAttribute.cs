92458b505f57b85bfe9181a4a15a42fc002ef1f4
ShvetsovAu <auskst@gmail.com>
2021-02-20 12:24:54 +0300

    P3DBAttribute entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/P3DBAttribute.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/P3DBAttribute.cs
@@ -1,22 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class P3DBAttribute
-    {
-        public P3DBAttribute()
-        {
-            Substitutions = new HashSet<Substitution>();
-        }
+    #region scaffold model
 
+    //public partial class P3DBAttribute
+    //{
+    //    public P3DBAttribute()
+    //    {
+    //        Substitutions = new HashSet<Substitution>();
+    //    }
+
+    //    public int ObjectId { get; set; }
+    //    public string Name { get; set; }
+    //    public string Alias { get; set; }
+    //    public bool UseSmartComparisonForSubstitution { get; set; }
+
+    //    public virtual ICollection<Substitution> Substitutions { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    /// <summary>
+    /// Атрибут 3д модели
+    /// </summary>
+    public class P3DBAttribute : IEntity
+    {
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public string Alias { get; set; }
+
         public bool UseSmartComparisonForSubstitution { get; set; }
 
+        /// <summary>
+        /// Таблица подстановок для атрибута
+        /// </summary>
         public virtual ICollection<Substitution> Substitutions { get; set; }
     }
 }
