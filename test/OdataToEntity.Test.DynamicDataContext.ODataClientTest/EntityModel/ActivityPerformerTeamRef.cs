14268390b4a4ab5a9ccc3ca44b1a119baaeb35e2
ShvetsovAu <auskst@gmail.com>
2021-02-20 09:01:44 +0300

    ActivityPerformerTeamRef entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ActivityPerformerTeamRef.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ActivityPerformerTeamRef.cs
@@ -1,18 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class ActivityPerformerTeamRef
-    {
-        public int ActivityId { get; set; }
-        public int PerformerTeamId { get; set; }
-        public int? PerformerTeamMembersCount { get; set; }
-        public string PerformerTeamMembers { get; set; }
+    #region scaffold model
 
+    //public partial class ActivityPerformerTeamRef
+    //{
+    //    public int ActivityId { get; set; }
+    //    public int PerformerTeamId { get; set; }
+    //    public int? PerformerTeamMembersCount { get; set; }
+    //    public string PerformerTeamMembers { get; set; }
+
+    //    public virtual Activity Activity { get; set; }
+    //    public virtual PerformerTeam PerformerTeam { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    /// <summary>
+    /// Связь работы с бригадой исполнителя
+    /// </summary>
+    //[Table("ActivityPerformerTeamRefs")]
+    public class ActivityPerformerTeamRef
+    {
+        /// <summary>
+        /// Ссылка на работу
+        /// </summary>
+        //[Key, Column(Order = 0)]
+        [ForeignKey("Activity")]
+        [Required]
+        public int ActivityId { get; set; }
         public virtual Activity Activity { get; set; }
-        public virtual PerformerTeam PerformerTeam { get; set; }
+
+        /// <summary>
+        /// Ссылка на бригаду исполнителей
+        /// </summary>
+        //[Key, Column(Order = 1)]
+        [ForeignKey("PerformerTeam")]
+        [Required]
+        public int PerformerTeamId { get; set; }
+        public PerformerTeam PerformerTeam { get; set; }
+
+        /// <summary>
+        /// Число членов бригады (заполняется в случае ручного редактирования бригады исполнителя)
+        /// </summary>
+        public int? PerformerTeamMembersCount { get; set; }
+
+        /// <summary>
+        /// Состав бригады (заполняется в случае ручного редактирования бригады исполнителя)
+        /// </summary>
+        public string PerformerTeamMembers { get; set; }
     }
 }
