5b1ad6382cd0177eaeaf393ea411a29aaa2b6a99
20912088 <an.shvetsov@ase-ec.ru>
2021-02-20 16:21:46 +0300

    ResourceCodeType entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ResourceCodeType.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/ResourceCodeType.cs
@@ -1,23 +1,60 @@
-﻿using System;
+﻿using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class ResourceCodeType
+    #region scaffold model
+
+    //public partial class ResourceCodeType
+    //{
+    //    public ResourceCodeType()
+    //    {
+    //        CodeResources = new HashSet<CodeResource>();
+    //    }
+
+    //    public int ObjectId { get; set; }
+    //    public bool IsSecureCode { get; set; }
+    //    public byte Length { get; set; }
+    //    public string Name { get; set; }
+    //    public int SequenceNumber { get; set; }
+
+    //    public virtual ICollection<CodeResource> CodeResources { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    //DB Valid
+    [Table("ResourceCodeType")]
+    public partial class ResourceCodeType : IEntity
     {
         public ResourceCodeType()
         {
-            CodeResources = new HashSet<CodeResource>();
+            CodeResources = new List<CodeResource>();
+            //this.ResourceCodes = new List<ResourceCode>();
         }
-
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
+        [Required]
         public bool IsSecureCode { get; set; }
+        [Required]
+        [Range(0, 20)]
         public byte Length { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; }
+        [Required]
         public int SequenceNumber { get; set; }
 
+        [InverseProperty("ResourceCodeType")]
         public virtual ICollection<CodeResource> CodeResources { get; set; }
+
+        //[InverseProperty("ResourceCodeType")]
+        //public virtual ICollection<ResourceCode> ResourceCodes { get; set; }
     }
 }
