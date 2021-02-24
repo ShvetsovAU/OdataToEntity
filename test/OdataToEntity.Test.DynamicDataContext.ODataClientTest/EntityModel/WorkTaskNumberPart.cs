5eeb5c4bd751720f7ba8e7880558c00702d86fff
ShvetsovAu <auskst@gmail.com>
2021-02-24 06:51:34 +0300

    WorkTaskNumberPart entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/WorkTaskNumberPart.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/WorkTaskNumberPart.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using OdataToEntity.Test.DynamicDataContext.ODataClientTest.Interfaces;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
-    public partial class WorkTaskNumberPart
+    #region scaffold model
+
+    //public partial class WorkTaskNumberPart
+    //{
+    //    public int ObjectId { get; set; }
+    //    public int Type { get; set; }
+    //    public string AvailableValues { get; set; }
+    //    public string DefaultValue { get; set; }
+    //    public string Delimiter { get; set; }
+    //    public int Index { get; set; }
+    //    public string FieldName { get; set; }
+    //    public int? StartIndex { get; set; }
+    //    public int? CharCount { get; set; }
+    //}
+
+    #endregion scaffold model
+
+    public partial class WorkTaskNumberPart : IEntity
     {
+        [Key]
+        [Required]
         public int ObjectId { get; set; }
+
+        [Required]
         public int Type { get; set; }
         public string AvailableValues { get; set; }
         public string DefaultValue { get; set; }
