c07e07dd59581bd8f1f1183c4f95d7d0010d5f6e
ShvetsovAu <auskst@gmail.com>
2021-02-24 06:53:26 +0300

    WorkType entity class for Odata EDM model generation modified

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/WorkType.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/EntityModel/WorkType.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
 namespace dbReverse.EntityModel
 {
+    #region scaffold model
+
+    //public partial class WorkType
+    //{
+    //    public short ObjectId { get; set; }
+    //    public string Name { get; set; }
+    //}
+
+    #endregion scaffold model
+
     public partial class WorkType
     {
+        [Key, Required]
         public short ObjectId { get; set; }
+
+        [MaxLength(200)]
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is WorkType entity))
+                return false;
+
+            return entity.ObjectId == ObjectId;
+        }
     }
 }
