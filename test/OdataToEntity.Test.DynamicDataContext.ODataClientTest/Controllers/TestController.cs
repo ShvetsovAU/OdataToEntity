95f49256af332afc581204345e0f9e42157f5907
ShvetsovAu <auskst@gmail.com>
2021-03-15 08:00:29 +0300

    projects references changed

--- test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/Controllers/TestController.cs
+++ test/OdataToEntity.Test.DynamicDataContext.ODataClientTest/Controllers/TestController.cs
@@ -1,5 +1,4 @@
-﻿using dbReverse.EntityModel;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.OData.Client;
 using Microsoft.OData.Edm;
 using Microsoft.OData.Edm.Csdl;
@@ -7,6 +6,7 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Xml;
+using ASE.MD.MDP2.Product.MDP2Service.Models.EntityModel;
 
 namespace OdataToEntity.Test.DynamicDataContext.ODataClientTest.Controllers
 {
@@ -40,7 +40,7 @@ namespace OdataToEntity.Test.DynamicDataContext.ODataClientTest.Controllers
             // Then generate an array of ObjectId
             //var res441 = dataSet4.Where(i => res43.Any(db => db.ObjectId == i.ObjectId)).ToList(); //TODO: not work
             //var res44 = dataSet4.Where(i => res43.Contains(i)).ToList();
-            var res43IdList = res43.Select(db => db.ObjectId).Take(1000).ToArray();//.ToList();//.Take(100).ToList(); //TODO: IList/List & ICollection/Collection are not currently supported
+            var res43IdList = res43.Select(db => db.ObjectId).Take(100).ToArray();//.ToList();//.Take(100).ToList(); //TODO: IList/List & ICollection/Collection are not currently supported
             var res442 = dataSet4.Where(i => res43IdList.Contains(i.ObjectId)).ToList();
             
             //var res23 = dataSet2.Where(i => i.ObjectId == 308398).ToList();
